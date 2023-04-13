39cc0c2412d66487693f001854217e7837f7e9a0
ZhenyaMalikov <130054976+ZhenyaMalikov@users.noreply.github.com>
2023-04-13 23:26:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,6 @@ from scipy.stats import anderson_ksamp
 chat_id = 516575251 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    
-    return anderson_ksamp([x, y]).pvalue < 0.03
+    p_val = stats.anderson_ksamp([x, y])[2]
+    alpha = 0.03
+    return p_val <= alpha
